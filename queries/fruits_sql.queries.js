aa97b133ef250d83d5ad4fdc318c27ee70b266af
Cristina Mateo <cris.mateo.99@gmail.com>
2024-02-07 18:07:09 +0100

    queries CRUD frutas

--- queries/fruits_sql.queries.js
+++ queries/fruits_sql.queries.js
@@ -3,7 +3,14 @@ getFruits: `SELECT nombre, imagen
 FROM frutas;`,
 getFruitbyName: `SELECT nombre, imagen, calorias, azucar, proteina, carbs, fibra
 FROM frutas
-WHERE nombre=$1;`
+WHERE nombre=$1;`,
+createFruit:`INSERT INTO frutas (nombre, imagen, calorias, proteina, azucar, carbs, fibra)
+VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+updateFruit:`UPDATE frutas
+SET image =$1
+WHERE nombre =$2;`,
+deleteFruit:`DELETE FROM frutas
+WHERE nombre = $1;`
 }
 
 module.exports = queries
