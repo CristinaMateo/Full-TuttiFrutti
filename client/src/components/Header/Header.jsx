b59d457c1aacd7bf45ff6053e451d187f3f53e0f
Cristina Mateo <cris.mateo.99@gmail.com>
2024-02-09 20:18:32 +0100

    header y nav

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+
+  
+  return (
+    <>
+      <nav>
+        <button id="log-out">Log out</button>
+        <button id="myspace">My Space</button>
+        <button id="signup-login">Sign up / Log in</button>
+      </nav>
+      <header>My Tutti Frutti App</header>
+    </>
+  );
+};
+
+export default Header;
