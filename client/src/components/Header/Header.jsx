f1a4db37357b5a019375709bc6b2131a00acfde9
Cristina Mateo <cris.mateo.99@gmail.com>
2024-02-11 11:27:54 +0100

    enrutamiento botones

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,22 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Logged from "./Logged/Logged";
 import { whoIsLogged } from "../Main/Authentication/utils";
+import Nav from "./Nav"
 
 
 const Header = () => {
   const loggedUser = whoIsLogged()
+  const navigate = useNavigate()
 
-  
+ const toAuth =()=>{
+  navigate('/auth')
+ }
 
   return (
     <>
       <section>
         {loggedUser && <Logged />}
-        {!loggedUser && <button id="signup-login">Sign up / Log in</button>}
+        {!loggedUser && <button id="signup-login" onClick={toAuth}>Sign up / Log in</button>}
       </section>
       <header>
         <h1>My Tutti Frutti App</h1>
-        
+        <Nav/>
       </header>
     </>
   );
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -3,14 +3,18 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Home from './Home'
 import Combos from './Combos'
 import Stats from './Stats'
+import MySpace from './Myspace'
+import Authentication from './Authentication'
 
 const Main = () => {
   return (
     <main>
 <Routes>
   <Route path="/" element={<Home/>}/> 
+  <Route path="/auth" element={<Authentication/>} />
   <Route path="/charts" element={<Stats/>}/> 
   <Route path="/opciones" element={<Combos/>}/> 
+  <Route path="/mySpace" element ={<MySpace/>}/>
   <Route path="/" element={<Navigate to={"/"} />}/> 
 </Routes>
     </main>
